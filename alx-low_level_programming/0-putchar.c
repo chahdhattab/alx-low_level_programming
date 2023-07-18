5683cbd465c504a9d3474e4a4d296d4d629fefa5
Your Name <you@example.com>
2023-07-18 15:15:21 -0700

    njfdbe

--- alx-low_level_programming/0-putchar.c
+++ alx-low_level_programming/0-putchar.c
@@ -1,17 +1,19 @@
-#include "main.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - prints the string "_putchar" from a character array.
- * Return: 0 on success.
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+printf("_putchar\n");
+
+return (0);
 }
+
--- alx-low_level_programming/main.h
+++ alx-low_level_programming/main.h
@@ -1,7 +1,4 @@
-/**
- * print_alphabt - print the lower-case alphabet on one line.
- */
-int _putchar(char c);
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(char c);
@@ -14,5 +11,3 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-
-
